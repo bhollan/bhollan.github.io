236d6d2f7012fdd2a2083422e4ae9a74b2ccfb78
Brian Holland <beehollander@gmail.com>
2015-04-06 14:44:46 -0500

    Push skeleton of all files

---
+++ StatWatch.js
@@ -0,0 +1,35 @@
+function testStatWatch() {
+	testArray = [1.1, 1.2, 1.3, 1.4, 1.0, 0.9, 0.88, 1.88, 1.9, 0.99, 1.1];
+	console.log(mean(testArray));
+	console.log(mode(testArray));
+	console.log(median(testArray));
+	console.log(stdDev(testArray));
+	console.log(variance(testArray));
+	
+}
+
+function mean(inputArray) {
+	return inputArray;
+}
+
+function mode(inputArray) {
+	return inputArray;
+}
+
+function median(inputArray) {
+	return inputArray;
+	
+}
+
+function stdDev(inputArray) {
+	return inputArray;
+}
+
+function variance(inputArray) {
+	return inputArray;
+}
+
+function markMoment(inputArray){
+	
+	return Date.now();
+}
